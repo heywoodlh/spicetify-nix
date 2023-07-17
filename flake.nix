42aca5ff70e12c1d1ece337d26ae43fe82f6377c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-07-17 11:38:05 -0600

    using latest nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,14 +6,16 @@
       url = "github:alindl/spicetify-themes";
       flake = false;
     };
-    nixpkgs.url = "nixpkgs/21.11";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, spicetify-themes }: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
       config.allowUnfree = true;
+      config.allowAliases = true;
     };
+    spotify-unwrapped = pkgs.spotify;
   in rec {
     packages.x86_64-linux = {
       solarizedDark = pkgs.callPackage ./spicetify.nix {
