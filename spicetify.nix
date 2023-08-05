9550f17a33bb1b62d484c34af6a468265079f6b7
Kyle Sferrazza <git@kylesferrazza.com>
2023-08-04 20:05:44 -0400

    remove pkgs input

--- spicetify.nix
+++ spicetify.nix
@@ -1,11 +1,8 @@
-{ pkgs, lib, buildGoModule, fetchFromGitHub, spotify, spicetify-themes, theme, colorscheme }:
-
-
-
-pkgs.stdenv.mkDerivation {
+{ stdenv, lib, buildGoModule, fetchFromGitHub, spotify, spicetify-cli, spicetify-themes, theme, colorscheme }:
+stdenv.mkDerivation {
   pname = "spotify";
   inherit (spotify) version;
-  src = pkgs.spotify;
+  src = spotify;
   doUnpackPhase = false;
 
   phases = [ "unpackPhase" "buildPhase" ];
@@ -13,19 +10,19 @@ pkgs.stdenv.mkDerivation {
   buildPhase = ''
     mkdir /tmp/spicetify-config
     export XDG_CONFIG_HOME=/tmp/spicetify-config
-    ${pkgs.spicetify-cli}/bin/spicetify-cli config 2>&1 > /dev/null || true
-    CFG_PATH=$(${pkgs.spicetify-cli}/bin/spicetify-cli -c)
+    ${spicetify-cli}/bin/spicetify-cli config 2>&1 > /dev/null || true
+    CFG_PATH=$(${spicetify-cli}/bin/spicetify-cli -c)
     sed -i "s:^spotify_path.*:spotify_path = $(pwd)/share/spotify:" $CFG_PATH
 
     touch /tmp/spicetify-config/prefs
     sed -i "s:^prefs_path.*:prefs_path = /tmp/spicetify-config/prefs:" $CFG_PATH
 
-    ${pkgs.spicetify-cli}/bin/spicetify-cli config current_theme ${theme}
-    ${pkgs.spicetify-cli}/bin/spicetify-cli config color_scheme ${colorscheme}
-    cat $(${pkgs.spicetify-cli}/bin/spicetify-cli -c)
+    ${spicetify-cli}/bin/spicetify-cli config current_theme ${theme}
+    ${spicetify-cli}/bin/spicetify-cli config color_scheme ${colorscheme}
+    cat $(${spicetify-cli}/bin/spicetify-cli -c)
     cp -R ${spicetify-themes}/${theme} /tmp/spicetify-config/spicetify/Themes/
-    ${pkgs.spicetify-cli}/bin/spicetify-cli backup apply || true
-    ${pkgs.spicetify-cli}/bin/spicetify-cli apply
+    ${spicetify-cli}/bin/spicetify-cli backup apply || true
+    ${spicetify-cli}/bin/spicetify-cli apply
     mkdir -p $out
     sed -i "s#${spotify}#$out#g" ./bin/spotify
     cp -r ./* $out
