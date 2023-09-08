e3e24c5fb0d6246ec30bb31d29af646440991658
Kyle Sferrazza <git@kylesferrazza.com>
2023-09-08 10:57:09 -0400

    nix fmt flake.nix

--- flake.nix
+++ flake.nix
@@ -14,45 +14,45 @@
     self,
     nixpkgs,
     spicetify-themes,
-    flake-utils
+    flake-utils,
   }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        config.allowAliases = true;
-      };
-      spotify-unwrapped = pkgs.spotify;
-    in {
-      formatter = pkgs.alejandra;
-      packages = rec {
-        dracula = pkgs.callPackage ./spicetify.nix {
-          theme = "Sleek";
-          colorscheme = "dracula";
-          inherit spicetify-themes;
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.allowAliases = true;
         };
-        dracula-text = pkgs.callPackage ./spicetify.nix {
-          theme = "text";
-          colorscheme = "dracula";
-          inherit spicetify-themes;
+        spotify-unwrapped = pkgs.spotify;
+      in {
+        formatter = pkgs.alejandra;
+        packages = rec {
+          dracula = pkgs.callPackage ./spicetify.nix {
+            theme = "Sleek";
+            colorscheme = "dracula";
+            inherit spicetify-themes;
+          };
+          dracula-text = pkgs.callPackage ./spicetify.nix {
+            theme = "text";
+            colorscheme = "dracula";
+            inherit spicetify-themes;
+          };
+          nord = pkgs.callPackage ./spicetify.nix {
+            theme = "Sleek";
+            colorscheme = "nord";
+            inherit spicetify-themes;
+          };
+          nord-text = pkgs.callPackage ./spicetify.nix {
+            theme = "text";
+            colorscheme = "nord";
+            inherit spicetify-themes;
+          };
+          solarized-text = pkgs.callPackage ./spicetify.nix {
+            theme = "text";
+            colorscheme = "solarized";
+            inherit spicetify-themes;
+          };
         };
-        nord = pkgs.callPackage ./spicetify.nix {
-          theme = "Sleek";
-          colorscheme = "nord";
-          inherit spicetify-themes;
-        };
-        nord-text = pkgs.callPackage ./spicetify.nix {
-          theme = "text";
-          colorscheme = "nord";
-          inherit spicetify-themes;
-        };
-        solarized-text = pkgs.callPackage ./spicetify.nix {
-          theme = "text";
-          colorscheme = "solarized";
-          inherit spicetify-themes;
-        };
-      };
-    }
-  );
+      }
+    );
 }
